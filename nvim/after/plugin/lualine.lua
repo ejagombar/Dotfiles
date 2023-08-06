cfa4b3ac01db57765f350a3761ec91a5618aa8e0
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2023-08-06 22:23:43 +0100

    show relative path lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -3,4 +3,13 @@ require('lualine').setup {
         component_separators = { left = '|', right = '|' },
         section_separators = { left = '', right = '' },
     },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                file_status = true, -- displays file status (readonly status, modified status)
+                path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+        }
+    },
 }
