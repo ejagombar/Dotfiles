e207b172b2a5718a58f8b88ffe72cbadaefc9139
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2023-08-07 22:44:53 +0100

    prettier fix part 12, season 3

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -1,5 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
+    print("null_ls not found")
     return
 end
 
@@ -7,11 +8,12 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- local diagnostics = null_ls.builtins.diagnostics
+vim.env.PRETTIERD_DEFAULT_CONFIG = vim.fn.expand('~/.prettierrc')
+
 
 null_ls.setup({
     debug = false,
     sources = {
-        formatting.prettierd.with({
-            extra_args = { "--tab-width", 4, "--no-semi", "--single-quote" } }),
+        formatting.prettierd -- .with({extra_args = { "--tab-width", 2, "--no-semi", "--single-quote" } }),
     },
 })
