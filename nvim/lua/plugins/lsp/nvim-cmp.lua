db65190f76899ec847757481ff4ddcefb46914b6
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-04-17 22:04:20 +0100

    remove efm

--- nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -89,9 +89,6 @@ return { -- Autocompletion
 						luasnip.jump(-1)
 					end
 				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			}),
 			sources = {
 				{ name = "nvim_lsp" },
