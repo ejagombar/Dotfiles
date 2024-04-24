acc722d0cc16a70649432b4f07db6539ecb3e78a
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-04-25 00:18:18 +0100

    fix formatting for cpp

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,12 +3,11 @@ return { -- Autoformat
 	lazy = false,
 	keys = {
 		{
-			"<leader>f",
+			"<leader>ff",
 			function()
 				require("conform").format({ async = true, lsp_fallback = true })
 			end,
 			mode = "",
-			desc = "[F]ormat buffer",
 		},
 	},
 	opts = {
@@ -17,7 +16,7 @@ return { -- Autoformat
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = {} -- { c = true, cpp = true }
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -26,11 +25,12 @@ return { -- Autoformat
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "black" },
+			python = { "isort", "black" },
+			cpp = { "clang-format" },
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
-			-- javascript = { { "prettierd", "prettier" } },
+			javascript = { { "prettierd", "prettier" } },
 		},
 	},
 }
