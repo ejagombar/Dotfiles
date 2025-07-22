01f74b7b25c9688ae7ab7a41e496532b9fb3723c
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2025-07-22 18:37:21 +0100

    Improvements to blink

--- nvim/lua/plugins/lsp/blink.lua
+++ nvim/lua/plugins/lsp/blink.lua
@@ -42,7 +42,6 @@ return { -- Autocompletion
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 			["<C-y>"] = { "accept", "fallback" },
 			["<C-Space>"] = { "show", "fallback" },
-			["<CR>"] = { "cancel", "fallback" },
 		},
 
 		appearance = {
@@ -78,7 +77,7 @@ return { -- Autocompletion
 		-- the rust implementation via `'prefer_rust_with_warning'`
 		--
 		-- See :h blink-cmp-config-fuzzy for more information
-		fuzzy = { implementation = "lua" },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 
 		-- Shows a signature help window while you type arguments for a function
 		signature = { enabled = true },
